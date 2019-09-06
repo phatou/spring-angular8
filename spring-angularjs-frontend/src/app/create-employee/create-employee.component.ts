8d2c83e990044a7672f607b078c2eab42ce94113
vinhp <phat.vinh@brusselsairport.be>
2019-09-06 14:56:32 +0200

    Fix input attribute in create employee template
    + re-order import in create-employee.component.ts

--- spring-angularjs-frontend/src/app/create-employee/create-employee.component.ts
+++ spring-angularjs-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
