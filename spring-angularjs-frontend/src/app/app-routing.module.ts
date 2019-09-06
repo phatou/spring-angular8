bec8c5da153c82e21b61fd34283b78bdf7b1463c
vinhp <phat.vinh@brusselsairport.be>
2019-09-06 14:56:56 +0200

    Add routes in app-routing.modules.ts

--- spring-angularjs-frontend/src/app/app-routing.module.ts
+++ spring-angularjs-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'employee', pathMatch: 'full'},
+  {path: 'employees', component: EmployeeListComponent},
+  {path: 'add', component: CreateEmployeeComponent},
+  {path: 'details/:id', component: EmployeeDetailsComponent},
+  {path: 'update/:id', component: UpdateEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
