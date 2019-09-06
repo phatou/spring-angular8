af069bdf01b584e9378000e4e6d35127eb4c999f
vinhp <phat.vinh@brusselsairport.be>
2019-09-06 14:55:04 +0200

    Add update employee from employee list route
    - Add update function to navigate to update employee route in employee
    - Add update employee button to trigger the navigation to update form

--- spring-angularjs-frontend/src/app/employee-list/employee-list.component.ts
+++ spring-angularjs-frontend/src/app/employee-list/employee-list.component.ts
@@ -35,6 +35,10 @@ export class EmployeeListComponent implements OnInit {
       );
   }
 
+  updateEmployee(id: number) {
+    this.router.navigate(['update', id]);
+  }
+
   employeeDetails(id: number) {
     this.router.navigate(['details', id]);
   }
